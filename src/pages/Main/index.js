c765609ee3464a986e060c589cc276a194e47e03
Diego Sousa <diego@diegosousa.com.br>
2020-09-28 07:27:00 -0300

    Add new repository to state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
 class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
   };
 
   handleInputChange = (event) => {
     this.setState({ newRepo: event.target.value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(this.state.newRepo);
+
+    const { newRepo } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...this.state.repositories, data],
+      newRepo: '',
+    });
   };
 
   render() {
